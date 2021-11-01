d3923458ee321852cebdff096030125b0ddbcb38
alexhuynh0530 <88806716+alexhuynh0530@users.noreply.github.com>
2021-11-01 00:38:23 -0500

    update challenge

--- scraping.py
+++ scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemispheres(browser)
     }
 
     # Stop webdriver and return data
@@ -97,6 +98,36 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+
+# Scrape High-Resolution Mars’ Hemisphere Images and Titles
+def hemispheres(browser):
+    # Visit URL
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # Add try/except for error handling
+    try:
+        # Find the relative image url
+        for i in range(4):
+            #create empty dictionary
+            hemispheres = {}
+            browser.find_by_tag('h3')[i].click()
+            element = browser.find_link_by_text('Sample').first
+            img_url = element['href']
+            title = browser.find_by_tag('h2')[0].text
+            hemispheres["img_url"] = img_url
+            hemispheres["title"] = title
+            hemisphere_image_urls.append(hemispheres)
+            browser.back()
+
+    except AttributeError:
+        return None
+
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
